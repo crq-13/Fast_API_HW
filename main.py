121fed1b2d3cacbb117843056e1dfe949dfc7ed9
Cristian Rojas <cristian@sioma.app>
2022-08-02 09:18:47 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+from typing import Optional
+from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+#Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_marriend: Optional[bool] = None
 
 @app.get("/")
 def home():
     return {"Hello": "world"}
+
+# Request and response body
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
+
