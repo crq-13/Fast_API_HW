ffc749aad135fdacb5d54d1e5625196f6d5efedf
Cristian Rojas <cristian@sioma.app>
2022-08-08 11:08:32 -0500

    summary and description

--- main.py
+++ main.py
@@ -88,9 +88,21 @@ def home():
     path="/person/new",
     response_model=PersonOut,
     status_code=status.HTTP_201_CREATED,
-    tags=["Persons"]
+    tags=["Persons"],
+    summary= "Create a person in the app"
     )
 def create_person(person: Person = Body(...)):
+    """
+    Create Person
+    ------
+    This function create a person in the app and save the information in the database
+    ------
+    Parameters:
+    - Request body parameter:
+        - **person: Person** -> A person model whit first name, last name, age, hair color and marital status
+
+    Returns a person model with first name, last name, age, hair color and marital status
+    """
     return person
 
 
